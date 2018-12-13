5efd0dfd16baca7d386d0a85bee2a74d1f7515dd
github username <github email address>
2018-12-13 13:39:03 -0500

    seeded some products and switching branches

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render json: @products
+  end
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,12 @@ Customer.create({name: "Ollie's", address: "456 Road Street", rank: 2, user_id: 
 Customer.create({name: "Max's Fun House", address: "1 Lane Street", rank: 3, user_id: 1})
 Customer.create({name: "Scoti's Magical Toy Shop", address: "007 Street Way", rank: 4, user_id: 1})
 
+Product.create({user_id: 1, name: "Barbie", category: "Dolls & Action Figures", sku: 1, cost: 5, price: 10})
+Product.create({user_id: 1, name: "Toy Story", category: "Movies", sku: 2, cost: 4, price: 9})
+Product.create({user_id: 1, name: "Mad Libs", category: "Books", sku: 3, cost: 2, price: 5})
+Product.create({user_id: 1, name: "Tomagachi", category: "Electronics", sku: 4, cost: 8, price: 15})
+Product.create({user_id: 1, name: "Monopolgy", category: "Board Games", sku: 5, cost: 6, price: 15})
+Product.create({user_id: 1, name: "Magic 8 Ball", category: "Toys", sku: 6, cost: 4, price: 10})
+
 
 Note.create({body: "Call Bob's"})
