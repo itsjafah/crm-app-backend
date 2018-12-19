1b50731b37cdfe7335bf25296a5bcf4f5e83fe56
github username <github email address>
2018-12-19 11:25:16 -0500

    seeded all products

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -5,4 +5,24 @@ class Api::V1::NotesController < ApplicationController
     render json: {notes: @notes}
   end
 
+  def create
+    if (@note = Note.create(note_params))
+      render json: @note, status: :created
+    else
+      render json: { errors: @note.errors}, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    @note = Note.find_by(id: params[:id])
+    @note.update(note_params)
+    render json: @note, status: 202
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:user_id, :customer_id, :body)
+  end
+
 end
