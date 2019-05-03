5b95fa78ca7c3e0d9dab68ff65e010b9e2f3446a
github username <github email address>
2019-05-03 13:01:23 -0400

    updated ruby

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,5 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :ordered_products
   has_many :products, through: :ordered_products
-# // custom instance method to calculate order total (price from product * quantity on order)
 
 end
